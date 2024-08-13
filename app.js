1af6b81692b418652a22a64644b2fe8aa7a9c3bd
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-08-13 13:14:47 +0530

    Some styling of main page

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ require("dotenv").config();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+
 
 app.get("/",(req,res) => {
     res.render("index",{messages:messages});
